146708a523fb787f0315451a1889c4191ffef66d
vejvarm <vejvarm@gmail.com>
2022-12-06 18:45:52 +0900

    feat: make unique global index for each description field

--- lab/csqa_statistics.py
+++ lab/csqa_statistics.py
@@ -16,15 +16,27 @@ if __name__ == '__main__':
 
     # read data and create csqa data dict
     # dict: partition -> folder -> file -> conversation
-    data_folder = f'{ROOT_PATH}{args.read_folder}/{args.partition}'
-    csqa_files = glob(f'{data_folder}/*/*.json')
+    data_folder = path.normpath(f'{ROOT_PATH}{args.read_folder}/{args.partition}')
+    csqa_files = glob(path.normpath(f'{data_folder}/*/*.json'))
     print(f'Reading folders for partition {args.partition}')
 
     d_qt_desc = dict()
 
+    # description to unique index mapping
+    path_to_desc2id_file = path.normpath(f"{ROOT_PATH}{args.read_folder}\\d_desc2id.json")
+    try:
+        with open(path_to_desc2id_file, 'r') as f:
+            d_desc2id = json.load(f)
+            unique_count = len(d_desc2id) - 1
+    except FileNotFoundError:
+        print(f"file '{path_to_desc2id_file}' doesn't exist. Creating empty dictionary.")
+        d_desc2id = dict()
+        unique_count = 0
+
     c_question_type = Counter()
     c_description = Counter()
     d_example = dict()
+    df_example_utterances = pandas.DataFrame(columns=['Question Type', 'Description', 'USER', 'SYSTEM'])
 
     for pth in csqa_files:
         pth = path.normpath(pth)
@@ -49,13 +61,25 @@ if __name__ == '__main__':
                 d_qt_desc[question_type] = Counter()
                 d_example[question_type] = {}
 
-            # capture first occurrence of each unique question-type+description combo as an example
-            if description not in d_example[question_type]:
-                d_example[question_type][description] = (entry_user, entry_system)
+            # capture all unique descriptions and assign them IDs
+            if description not in d_desc2id.keys():
+                d_desc2id[description] = unique_count
+                unique_count += 1
 
             c_question_type.update([question_type])
             c_description.update([description])
-            d_qt_desc[question_type].update([description])
+            d_qt_desc[question_type].update([d_desc2id[description]])
+
+            # capture first occurrence of each unique question-type+description combo as an example
+            if description not in d_example[question_type]:
+                d_example[question_type][description] = (entry_user, entry_system)
+                df_row = pandas.DataFrame({'Question Type': question_type,
+                                           'Description': description,
+                                           'USER': entry_user['utterance'],
+                                           'SYSTEM': entry_system['utterance']},
+                                          index=[int(f"{entry_user['ques_type_id']}{d_desc2id[description]:02d}")])
+                df_example_utterances = pandas.concat([df_example_utterances, df_row])
+
     print(f'Found {len(d_qt_desc.keys())} unique question-types and {len(c_description.keys())} uniqe descriptions.')
 
     print(f'Creating and saving contingecy table with Qtype/description:')
@@ -63,11 +87,18 @@ if __name__ == '__main__':
     df.to_csv(f'{data_folder}\\qt_desc_contingency.csv')
 
     print(f'Plotting contingency table as bar plot and saving to {data_folder}')
-    df.plot(kind="bar", stacked=True, legend=True)
+    df.plot(kind="bar", stacked=True, legend=True, xlabel='description id', ylabel='number of entries')
 
     print(f'Saving d_example.json')
     with open(f"{data_folder}\\d_example.json", 'w', encoding='utf8') as f:
         json.dump(d_example, f, indent=4, ensure_ascii=False)
+
+    print(f'Saving d_desc2id legend to d_desc2id.json')
+    with open(path_to_desc2id_file, 'w', encoding='utf8') as f:
+        json.dump(d_desc2id, f, indent=4, ensure_ascii=False)
+
+    print(f'Saving df_example_utterances.csv')
+    df_example_utterances.to_csv(f"{data_folder}\\df_example_utterances.csv")
 
     plt.savefig(f'{data_folder}\\qt_desc_contingency.pdf')
     plt.savefig(f'{data_folder}\\qt_desc_contingency.png', dpi=200)
