bd345100b7f75a859abb89fe6531405f6224e57c
vejvarm <vejvarm@gmail.com>
2022-12-06 17:14:52 +0900

    feat: construct json file with example for each q-type+description

--- lab/csqa_statistics.py
+++ lab/csqa_statistics.py
@@ -24,6 +24,7 @@ if __name__ == '__main__':
 
     c_question_type = Counter()
     c_description = Counter()
+    d_example = dict()
 
     for pth in csqa_files:
         pth = path.normpath(pth)
@@ -33,18 +34,28 @@ if __name__ == '__main__':
         with open(pth) as json_file:
             data_list = json.load(json_file)
 
-        for entry in data_list:
-            if entry['speaker'] == 'SYSTEM':
+        for i in range(len(data_list)//2):
+            entry_user = data_list[2*i]  # USER
+            entry_system = data_list[2*i + 1]  # SYSTEM
+
+            if 'description' not in entry_user.keys():
                 continue
 
-            # entry['speaker'] == 'USER':
-            if entry['question-type'] not in d_qt_desc.keys():
-                d_qt_desc[entry['question-type']] = Counter()
+            question_type = entry_user['question-type']
+            description = entry_user['description']
 
-            c_question_type.update([entry['question-type']])
-            if 'description' in entry.keys():
-                c_description.update([entry['description']])
-                d_qt_desc[entry['question-type']].update([entry['description']])
+            # entry['speaker'] == 'USER':
+            if question_type not in d_qt_desc.keys():
+                d_qt_desc[question_type] = Counter()
+                d_example[question_type] = {}
+
+            # capture first occurrence of each unique question-type+description combo as an example
+            if description not in d_example[question_type]:
+                d_example[question_type][description] = (entry_user, entry_system)
+
+            c_question_type.update([question_type])
+            c_description.update([description])
+            d_qt_desc[question_type].update([description])
     print(f'Found {len(d_qt_desc.keys())} unique question-types and {len(c_description.keys())} uniqe descriptions.')
 
     print(f'Creating and saving contingecy table with Qtype/description:')
@@ -53,6 +64,10 @@ if __name__ == '__main__':
 
     print(f'Plotting contingency table as bar plot and saving to {data_folder}')
     df.plot(kind="bar", stacked=True, legend=True)
+
+    print(f'Saving d_example.json')
+    with open(f"{data_folder}\\d_example.json", 'w', encoding='utf8') as f:
+        json.dump(d_example, f, indent=4, ensure_ascii=False)
 
     plt.savefig(f'{data_folder}\\qt_desc_contingency.pdf')
     plt.savefig(f'{data_folder}\\qt_desc_contingency.png', dpi=200)
