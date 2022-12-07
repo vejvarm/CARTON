9afd4911a0f3b76c30d9e8ab34b6b35b8f78ca91
vejvarm <vejvarm@gmail.com>
2022-12-07 16:17:19 +0900

    refactor: use regexp for functionality of active_set replacements

--- lab/expand_csqa.py
+++ lab/expand_csqa.py
@@ -1,13 +1,16 @@
 import json
+import logging
+import re
 from pathlib import Path
 
 from constants import args, ROOT_PATH, ENTITY, TYPE, RELATION
 
+
 from action_executor.actions import ESActionOperator
-from helpers import connect_to_elasticsearch
+from helpers import connect_to_elasticsearch, setup_logger
 
-CLIENT = connect_to_elasticsearch()
-
+# CLIENT = connect_to_elasticsearch()
+LOGGER = setup_logger(__name__, loglevel=logging.DEBUG)
 
 def decode_active_set(active_set: list[str]):
     decoded_list = []
@@ -27,6 +30,31 @@ def decode_active_set(active_set: list[str]):
         decoded_list.append(list(zip([sub, rel, obj], category)))
 
     return decoded_list
+
+
+def fill_active_set_with_entities(active_set: list[str], entities: list[str], op: ESActionOperator):
+    decoded_list = []
+    pattern = r'c\(.+?\)'
+    d_type2ent = {}
+    for ent in entities:
+        for tp in op.get_types(ent):
+            if tp not in d_type2ent.keys():
+                d_type2ent[tp] = [ent]
+            else:
+                d_type2ent[tp].append(ent)
+
+    LOGGER.debug(f'd_type2ent in fill_active_set_with_entities: {d_type2ent}')
+
+    for entry in active_set:
+        LOGGER.debug(f'entry in fill_active_set_with_entities: {entry}')
+        for tp, ent_list in d_type2ent.items():
+            entry = re.sub(rf'c\({tp}\)', ent_list.pop(), entry, 1)  # ONLY ONE REPLACEMENT MADE per entity
+            LOGGER.debug(f'entry (out) in fill_active_set_with_entities: {entry}')
+
+        decoded_list.append([entry])
+
+    return decoded_list
+
 
 if __name__ == "__main__":
     # pop unneeded conversations right here?
@@ -57,39 +85,11 @@ if __name__ == "__main__":
 
             if 'Simple' in entry_user['question-type']:
                 print(f"USER: {entry_user['question-type']}, {entry_user['entities_in_utterance']}, {entry_user['utterance']}")
-                print(f"SYSTEM: {entry_system['entities_in_utterance']}, {entry_system['active_set']} {entry_system['utterance']}")
+                print(f"SYSTEM: {entry_system['all_entities']}, {entry_system['entities_in_utterance']}, {entry_system['active_set']} {entry_system['utterance']}")
 
-                active_set_decoded = decode_active_set(entry_system['active_set'])
-                print(active_set_decoded)
-
-                new_active_set = []
-                for entry in active_set_decoded:
-                    sub, sub_cat = entry[0]
-                    rel, rel_cat = entry[1]
-                    obj, obj_cat = entry[2]
-
-                    if sub_cat == TYPE and obj_cat == TYPE:
-                        raise NotImplementedError('Both subject and object are of category TYPE.')
-
-                    if sub_cat == TYPE:
-                        subs = set(op.filter_type(op.find_reverse(obj, rel), sub)).intersection(entry_system['entities_in_utterance'])
-                        new_active_set.append([f"i({e},{rel},{obj})" for e in subs])
-
-                    if obj_cat == TYPE:
-                        objs = set(op.filter_type(op.find(sub, rel), obj)).intersection(entry_system['entities_in_utterance'])
-                        new_active_set.append([f"i({sub},{rel},{e})" for e in objs])
-
-                print(new_active_set)
+                new_active_set = fill_active_set_with_entities(entry_system['active_set'], entry_system['entities_in_utterance'], op)
+                LOGGER.debug(f'new_active_set in {__name__}: {new_active_set}')
                 print(f"".center(50, "-"), end='\n\n')
-
-            # active_set
-            # '(Q1, P1, c(Q2))' ... take all entities with type c(Q2) and cross reference them with entities in utterance
-            # essentially
-            # for each entity in filter_type(find(Q1, P1), Q2)
-            # actually just for each entity E in sysyem['entities_in_utterance']: get(Q1, P1, E) where E replaces the c() part
-            # if _id Q1P1E exists: append '(Q1, P1, E)' to active_set list
-
-
 
             # conversation types to tweak:
             # and how?
