c61e3330f58c9f86ab561e043213e5a05782a807
vejvarm <vejvarm@gmail.com>
2022-12-12 15:10:39 +0900

    fix: change pth key to pth.parent.name

--- lab/expand_csqa.py
+++ lab/expand_csqa.py
@@ -318,7 +318,7 @@ def compare_generated_utterances(model_choices: list[QA2DModelChoices] or QA2DMo
         results[model_choice.name] = {}
 
         for pth in csqa_files:
-            results[model_choice.name][pth] = {}
+            results[model_choice.name][pth.parent.name] = {}
 
             with open(pth, encoding='utf8') as json_file:
                 conversation = json.load(json_file)
@@ -330,11 +330,11 @@ def compare_generated_utterances(model_choices: list[QA2DModelChoices] or QA2DMo
                 if 'Simple' not in entry_user['question-type']:
                     continue
 
-                if entry_user['question-type'] not in results[model_choice.name][pth].keys():
-                    results[model_choice.name][pth][entry_user['question-type']] = {}
+                if entry_user['question-type'] not in results[model_choice.name][pth.parent.name].keys():
+                    results[model_choice.name][pth.parent.name][entry_user['question-type']] = {}
 
-                if entry_user['description'] not in results[model_choice.name][pth][entry_user['question-type']].keys():
-                    results[model_choice.name][pth][entry_user['question-type']][entry_user['description']] = {
+                if entry_user['description'] not in results[model_choice.name][pth.parent.name][entry_user['question-type']].keys():
+                    results[model_choice.name][pth.parent.name][entry_user['question-type']][entry_user['description']] = {
                         'utterances': (entry_user['utterance'], entry_system['utterance']),
                         'entities': (entry_user['entities_in_utterance'], entry_system['entities_in_utterance'])
                     }
@@ -345,10 +345,10 @@ def compare_generated_utterances(model_choices: list[QA2DModelChoices] or QA2DMo
                     LOGGER.info(f'statement: {statement}')
                     LOGGER.info(f"".center(50, "-") + "\n\n")
 
-                    if labels_as.name not in results[model_choice.name][pth][entry_user['question-type']][entry_user['description']]:
-                        results[model_choice.name][pth][entry_user['question-type']][entry_user['description']][labels_as.name] = {}
+                    if labels_as.name not in results[model_choice.name][pth.parent.name][entry_user['question-type']][entry_user['description']]:
+                        results[model_choice.name][pth.parent.name][entry_user['question-type']][entry_user['description']][labels_as.name] = {}
 
-                    results[model_choice.name][pth][entry_user['question-type']][entry_user['description']][labels_as.name] = {
+                    results[model_choice.name][pth.parent.name][entry_user['question-type']][entry_user['description']][labels_as.name] = {
                         'statement': statement
                     }
 
