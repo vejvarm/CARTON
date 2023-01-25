b6ecb6264e36b63f67698f30d7f744aeb4862e95
vejvarm@freya <vejvarm@gmail.com>
2023-01-25 15:58:21 +0900

    feat: add "table_format" and "insert_active_set" entries to csqa dataset

--- annotate_csqa/transform_active_set.py
+++ annotate_csqa/transform_active_set.py
@@ -3,6 +3,7 @@ import logging
 import re
 
 from pathlib import Path
+from tqdm import tqdm
 from action_executor.actions import ESActionOperator
 from helpers import connect_to_elasticsearch, setup_logger
 from constants import ROOT_PATH
@@ -10,7 +11,7 @@ from args import parse_and_get_args
 args = parse_and_get_args()
 
 CLIENT = connect_to_elasticsearch()
-LOGGER = setup_logger(__name__, loglevel=logging.INFO)
+LOGGER = setup_logger(__name__, loglevel=logging.WARNING)
 
 
 class ActiveSetTransformer:
@@ -55,29 +56,53 @@ class ActiveSetTransformer:
 
         return tab_dict
 
+    def tab_dict_to_jsonl(self, tab_dict: dict[tuple[str]: list[str]]):
+        line = []
+        for (subj, rel), objs in tab_dict.items():
+            obs = [op.get_entity_label(o).split(" ") for o in objs]
+            line.extend([["name", op.get_entity_label(subj).split(" ")],
+                         [op.get_relation_label(rel), [o for ob in obs for o in ob]]])
 
-if __name__ == "__main__":
-    read_folder = "data/final_simple/csqa"
-    partition = "train"
-    data_folder = ROOT_PATH.joinpath(read_folder).joinpath(partition)
-    csqa_files = data_folder.glob('**/d_dataset_like_example_file.json')
-    LOGGER.info(f'Reading folders for partition {partition}')
+        LOGGER.debug(f"line: {line}")
+        return line
+
+
+def transform_active_set(data_folder: Path):
+    csqa_files = data_folder.glob('**/*.json')
 
     op = ESActionOperator(CLIENT)
     actset_builder = ActiveSetTransformer(op)
 
-    for pth in csqa_files:
+    for pth in tqdm(csqa_files):
         with pth.open("r", encoding="utf8") as json_file:
             conversation = json.load(json_file)
 
         for i in range(0, len(conversation), 2):
             user = conversation[i]
             system = conversation[i+1]
-
+            LOGGER.info("\n")
+            LOGGER.debug(f"turn {user['turn_position']} in json_file: {pth.parent.name}/{pth.name}")
             new_active_set = actset_builder.build_active_set(user, system)
             LOGGER.info(f"active_set: {new_active_set}")
 
             tab_dict = actset_builder.build_table(new_active_set)
             LOGGER.info(f"tab_dict: {tab_dict}")
 
-            system["active_set"] = new_active_set
+            table = actset_builder.tab_dict_to_jsonl(tab_dict)
+
+            system["insert_active_set"] = new_active_set
+            system["table_format"] = table
+
+        with pth.open("w", encoding="utf8") as json_file:
+            json.dump(conversation, json_file, indent=4)
+
+
+if __name__ == "__main__":
+    partitions = {"test", "train", "val"}
+    read_folder = "data/final_simple/csqa"
+
+    for partition in partitions:
+        data_folder = ROOT_PATH.joinpath(read_folder).joinpath(partition)
+
+        LOGGER.info(f'Reading folders for partition {partition}')
+        transform_active_set(data_folder)
