2c2e8391f30e8feeddbc6f51131beaf564a1c26e
vejvarm@freya <vejvarm@gmail.com>
2023-11-27 21:10:15 +0900

    refactor: add KeyError handling for DataBatch

--- dataset.py
+++ dataset.py
@@ -58,8 +58,11 @@ class DataBatch:
             coref.append(self._tensor([vocabs[COREF].stoi[s] for s in sample[4]]))
             predicate_pointer.append(self._tensor([vocabs[PREDICATE_POINTER].stoi[s] for s in sample[5]]))
             type_pointer.append(self._tensor([vocabs[TYPE_POINTER].stoi[s] for s in sample[6]]))
-            entity_pointer.append(self._tensor([vocabs[ENTITY].stoi[s] for s in sample[7]]))
-
+            try:
+                entity_pointer.append(self._tensor([vocabs[ENTITY].stoi[s] for s in sample[7]]))
+            except KeyError as err:
+                repr(err)  # !HACK
+                entity_pointer.append(self._tensor([vocabs['NA'].stoi[s] for s in sample[7]]))
         self.id = self._tensor(id).to(device)
         self.input = pad_sequence(inp,
                                   padding_value=vocabs[INPUT].stoi[PAD_TOKEN],
