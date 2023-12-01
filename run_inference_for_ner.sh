1a81e3b4a1aef5440f802618629c2e00aeaf1ef8
vejvarm@freya <vejvarm@gmail.com>
2023-12-01 21:15:52 +0900

    chore: change path for CARTONNER trained on merged for 15 epochs

--- run_inference_for_ner.sh
+++ run_inference_for_ner.sh
@@ -24,8 +24,8 @@ python inference_for_ner_recall.py --name "02_csqa15_on_d2t" --batch-size 25 --m
 
 
 # CARTONNER trained on MERGED dataset for 15 epochs
-python inference_for_ner_recall.py --name "03_merged15_on_csqa" --batch-size 25 --model-path experiments/models/CARTONNER_merged_e15_v0.0146_multitask.pth.tar --data-path data/final/csqa --cuda-device 1
+python inference_for_ner_recall.py --name "03_merged15_on_csqa" --batch-size 25 --model-path experiments/models/CARTONNER_merged15_e15_v0.0273_multitask.pth.tar --data-path data/final/csqa --cuda-device 1
 
-python inference_for_ner_recall.py --name "03_merged15_on_merged" --batch-size 25 --model-path experiments/models/CARTONNER_merged_e15_v0.0146_multitask.pth.tar --data-path data/csqa-merged --cuda-device 1
+python inference_for_ner_recall.py --name "03_merged15_on_merged" --batch-size 25 --model-path experiments/models/CARTONNER_merged15_e15_v0.0273_multitask.pth.tar --data-path data/csqa-merged --cuda-device 1
 
-python inference_for_ner_recall.py --name "03_merged15_on_d2t" --batch-size 25 --model-path experiments/models/CARTONNER_merged_e15_v0.0146_multitask.pth.tar --data-path data/d2t-sampled --cuda-device 1
+python inference_for_ner_recall.py --name "03_merged15_on_d2t" --batch-size 25 --model-path experiments/models/CARTONNER_merged15_e15_v0.0273_multitask.pth.tar --data-path data/d2t-sampled --cuda-device 1
