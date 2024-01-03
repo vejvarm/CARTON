e91d1455852a224fd827bbfb3a36fec2a731d181
vejvarm@freya <vejvarm@gmail.com>
2024-01-03 10:11:48 +0900

    chore: update models with padded versions

--- run_inference.sh
+++ run_inference.sh
@@ -24,8 +24,8 @@ python inference.py --name "02_csqa15_on_d2t" --batch-size 50 --model-path exper
 
 
 # CARTONNER trained on MERGED dataset for 15 epochs
-python inference.py --name "03_merged15_on_csqa" --batch-size 50 --model-path experiments/models/CARTONNER_merged15_e15_v0.0273_multitask.pth.tar --data-path data/final/csqa --cuda-device 0
+python inference.py --name "03_merged15_on_csqa" --batch-size 50 --model-path experiments/models/CARTONNER_merged15_e15_v0.0258_multitask.pth.tar --data-path data/final/csqa --cuda-device 0
 
-python inference.py --name "03_merged15_on_merged" --batch-size 50 --model-path experiments/models/CARTONNER_merged15_e15_v0.0273_multitask.pth.tar --data-path data/csqa-merged --cuda-device 0
+python inference.py --name "03_merged15_on_merged" --batch-size 50 --model-path experiments/models/CARTONNER_merged15_e15_v0.0258_multitask.pth.tar --data-path data/csqa-merged --cuda-device 0
 
-python inference.py --name "03_merged15_on_d2t" --batch-size 50 --model-path experiments/models/CARTONNER_merged15_e15_v0.0273_multitask.pth.tar --data-path data/d2t-sampled --cuda-device 0
+python inference.py --name "03_merged15_on_d2t" --batch-size 50 --model-path experiments/models/CARTONNER_merged15_e15_v0.0258_multitask.pth.tar --data-path data/d2t-sampled --cuda-device 0
